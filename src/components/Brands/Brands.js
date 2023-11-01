eb73f017694ecf2b6d00192abc98a74a5b98095c
HoangLong2608 <lehoanglong260898@gmail.com>
2023-11-01 14:51:48 +0700

    sửa category vs brand

--- src/components/Brands/Brands.js
+++ src/components/Brands/Brands.js
@@ -7,11 +7,50 @@ import Products from "../Data.json";
 
 function Brands() {
   const { id } = useParams();
-  let items = Products.filter((items) => items.Brand.id === id);
+  let items = Products.filter((items) => items.Brand.id == id);
   console.log(items);
   return (
     <div>
-     <h1>Brands</h1> 
+     <Container>
+      <Row>
+        {items.map((items, index) => (
+            <Col xs={12} sm={6} md={6} lg={3} key={index}>
+              <Card className='card-newarrivals'>
+                
+                  <Card.Img
+                      className='img-product'
+                      variant="top"
+                      src={items.img1}
+                      alt={items.name}
+                  />
+          
+                <Card.Body> 
+                    <Card.Title className="title-card-products">
+                      {items.name}
+                    </Card.Title>
+
+                    <Card.Text className="price-card-products">
+                      ${items.price}
+                    </Card.Text>
+                    <Card.Text className="star-card-products">
+                      <i class="fa-sharp fa-solid fa-star"></i>
+                      <i class="fa-sharp fa-solid fa-star"></i>
+                      <i class="fa-sharp fa-solid fa-star"></i>
+                      <i class="fa-sharp fa-solid fa-star"></i>
+                      <i class="fa-solid fa-star-half-stroke"></i>
+                    </Card.Text>
+
+                  <Button
+                    className="add-products"
+                  >
+                    ADD TO CART
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+      </Row>
+    </Container> 
     </div>
   )
 }
--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -8,11 +8,50 @@ import Products from "../Data.json";
 
 function Categories() {
   const {id} = useParams();
-  let item = Products.filter((item) => item.Category.id === id);
-  console.log(item);
+  let items = Products.filter((items) => items.Category.id == id);
+  console.log(items);
 
   return (
-    <h1>Categories</h1>
+    <Container>
+      <Row>
+        {items.map((items, index) => (
+            <Col xs={12} sm={6} md={6} lg={3} key={index}>
+              <Card className='card-newarrivals'>
+                
+                  <Card.Img
+                      className='img-product'
+                      variant="top"
+                      src={items.img1}
+                      alt={items.name}
+                  />
+          
+                <Card.Body> 
+                    <Card.Title className="title-card-products">
+                      {items.name}
+                    </Card.Title>
+
+                    <Card.Text className="price-card-products">
+                      ${items.price}
+                    </Card.Text>
+                    <Card.Text className="star-card-products">
+                      <i class="fa-sharp fa-solid fa-star"></i>
+                      <i class="fa-sharp fa-solid fa-star"></i>
+                      <i class="fa-sharp fa-solid fa-star"></i>
+                      <i class="fa-sharp fa-solid fa-star"></i>
+                      <i class="fa-solid fa-star-half-stroke"></i>
+                    </Card.Text>
+
+                  <Button
+                    className="add-products"
+                  >
+                    ADD TO CART
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+      </Row>
+    </Container>
   )
 }
 
